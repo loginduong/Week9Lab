cd4592bcae406d940a8d5cb9d607e667b61ab3de
831690 <831690@SAIT224319IT.ACDM.DS.SAIT.CA>
2020-11-04 08:47:42 -0700

    RoleDB.java updated

--- Week8Lab/src/java/dataaccess/RoleDB.java
+++ Week8Lab/src/java/dataaccess/RoleDB.java
@@ -1,39 +1,20 @@
 package dataaccess;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
+
 import java.util.List;
+import javax.persistence.EntityManager;
 import models.Role;
 
 public class RoleDB {
 
     public List<Role> getAll() throws Exception {
-        List<Role> roles = new ArrayList<>();
-        ConnectionPool cp = ConnectionPool.getInstance();
-        Connection con = cp.getConnection();
-        PreparedStatement ps = null;
-        ResultSet rs = null;
+        EntityManager em = DBUtil.getEntityManagerFactory().createEntityManager();
         
-        String sql = "SELECT * FROM role";
         
         try {
-            ps = con.prepareStatement(sql);
-            rs = ps.executeQuery();
-            while (rs.next()) {
-                int roleId = rs.getInt(1);
-                String roleName = rs.getString(2);
-                Role role = new Role(roleId, roleName);
-                roles.add(role);
-            }
+            List <Role> role = em.createNamedQuery("Role.findAll", Role.class);
+            return role;
         } finally {
-            DBUtil.closeResultSet(rs);
-            DBUtil.closePreparedStatement(ps);
-            cp.freeConnection(con);
+            em.close();
         }
-
-        return roles;
-    }
-
 }
